90a1eb57b490bb075fee378b66eb97db108c03ea
Sidpark <kernelconsultants@gmail.com>
2023-10-18 22:58:40 +0530

    Code 1

---
+++ app.js
@@ -0,0 +1,7 @@
+const Heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(Heading);
